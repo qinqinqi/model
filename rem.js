dbdb04adae234cad25fb6c08e7b4fca8fc3192ec
qinqinqi <36070736+qinqinqi@users.noreply.github.com>
2019-03-06 11:14:53 +0800

    Add files via upload

---
+++ rem.js
@@ -0,0 +1,16 @@
+var fontSize
+(function(doc,win){
+	
+var docEl=doc.documentElement,resizeEvt='orientationchange'in window?'orientationchange':'resize',recalc=function(){
+	  var clientWidth=docEl.clientWidth;
+	  fontSize = 100*(clientWidth/６４０)
+	  if(fontSize>=100){
+		  fontSize=100
+	  }
+	  if(!clientWidth)return;
+	  docEl.style.fontSize=fontSize+'px'
+	  docEl.style.display = 'block'
+	};
+	if(!doc.addEventListener)return;win.addEventListener(resizeEvt,recalc,false);
+	doc.addEventListener('DOMContentLoaded',recalc,false)
+})(document,window);
